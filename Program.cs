6bb6677dbbe4a422546af2638ae611bbf5cd5948
AkshatPatel155 <106282937+AkshatPatel155@users.noreply.github.com>
2022-06-16 10:58:12 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using ConsoleApp3.Models;
+
+
+string host = "ftp://waws-prod-yt1-039.ftp.azurewebsites.windows.net//22S_BDAT1001/Section_2/BDAT1001_30632";
+string name = "ftpservernital\bdat1001-user";
+string password = "Bdat1001#";
+
+DirectoryList obj = new DirectoryList();
+obj.GetDirectoryList(host,name,password);
